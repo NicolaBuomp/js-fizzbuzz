d3c82e6f65ea1e9e079d866be4e391c493fe359f
Nicola <niconix.dev@gmail.com>
2020-04-01 19:14:08 +0200

    html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
+var testo = document.getElementById('text');
+var testoDue = '';
 for (var i = 1; i < 101; i++) {
 
-    if (i % 3 == 0 && i % 5 != 0) {
-        console.log('Fizz')
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz')
+        testoDue += '<li>FizzBuzz</li>'
+    } else if (i % 3 == 0 && i % 5 != 0) {
+        console.log('Fizz');
+        testoDue += '<li>Fizz</li>'
     } else if (i % 5 == 0 && i % 3 != 0) {
         console.log('Buzz')
-    } else if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz')
+        testoDue += '<li>Buzz</li>'
     } else {
         console.log(i)
+        testoDue += '<li>' + i + '</li>'
     }
 }
+
+testo.innerHTML = testoDue;
